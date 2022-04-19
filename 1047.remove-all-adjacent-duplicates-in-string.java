70fc194f4c9dae440135d3a90c299438688d0e95
Charles <mcrichard.buaa@gmail.com>
2022-04-19 23:27:23 +0000

    1047 1

--- 1047.remove-all-adjacent-duplicates-in-string.java
+++ 1047.remove-all-adjacent-duplicates-in-string.java
@@ -7,18 +7,34 @@
 // @lc code=start
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character> stack = new Stack<Character>();
+        // Stack<Character> stack = new Stack<Character>();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!stack.isEmpty() && stack.peek() == c) stack.pop();
-            else stack.push(c);
+        // for (int i = 0; i < s.length(); i++) {
+        //     char c = s.charAt(i);
+        //     if (!stack.isEmpty() && stack.peek() == c) stack.pop();
+        //     else stack.push(c);
+        // }
+
+        // StringBuilder sb = new StringBuilder();
+        // while (!stack.isEmpty()) sb.insert(0, stack.pop());
+
+        // return sb.toString();
+
+        int p = 0;
+        char[] chars = s.toCharArray();
+
+        for (int i = 1; i < chars.length; i++) {
+            char c = chars[i];
+
+            if (p == -1 || chars[p] != c) {
+                chars[++p] = c;
+            } else {
+                p--;
+            }
         }
 
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) sb.insert(0, stack.pop());
-
-        return sb.toString();
+        if (p == -1) return "";
+        return new String(Arrays.copyOfRange(chars, 0, p + 1));
     }
 }
 // @lc code=end
